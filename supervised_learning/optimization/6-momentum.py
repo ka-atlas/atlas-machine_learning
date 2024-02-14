669140b8433e061d497d5aa4f632045882ce4edb
kush-holberton <124093082+kush-holberton@users.noreply.github.com>
2024-02-14 09:06:00 -0600

    Update 6-momentum.py

--- supervised_learning/optimization/6-momentum.py
+++ supervised_learning/optimization/6-momentum.py
@@ -1,16 +1,7 @@
 #!/usr/bin/env python3
-"""module documentation
-this module creates a function.
-
-"""
 import tensorflow.compat.v1 as tf
 
 
 def create_momentum_op(loss, alpha, beta1):
-    """
-    loss = loss of the network
-    alpha = learning rate
-    beta1 = momentum weight
-    """
     optimizer = tf.train.MomentumOptimizer(alpha, beta1).minimize(loss)
     return optimizer
