0eaa1c61c1cd8687513b22e960be580ec205a3d0
kush-holberton <124093082+kush-holberton@users.noreply.github.com>
2024-02-14 14:21:08 -0600

    Update 6-momentum.py

--- supervised_learning/optimization/6-momentum.py
+++ supervised_learning/optimization/6-momentum.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import tensorflow.compat.v1 as tf
+tf.disable_eager_execution()
 
 def create_momentum_op(loss, alpha, beta1):
     return tf.train.MomentumOptimizer(alpha, beta1).minimize(loss)
